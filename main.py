50004834dc3c7ecd00c143265134b390fe6b4036
Yawen Li <yawenl@mun.ca>
2023-10-07 11:44:40 -0230

    init

---
+++ main.py
@@ -0,0 +1,45 @@
+import requests
+import json
+import datetime
+import os
+import socket
+from dotenv import load_dotenv
+
+# Load the .env file
+load_dotenv()
+
+GITHUB_RAW_URL = os.getenv('JSON_URL')
+
+def fetch_json_from_github(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
+
+def is_time_within_period(start, end, check_time):
+    start_time = datetime.datetime.strptime(start, "%H:%M").time()
+    end_time = datetime.datetime.strptime(end, "%H:%M").time()
+    return start_time <= check_time <= end_time
+
+def should_shutdown(computers_data, computer_name):
+    current_time = datetime.datetime.now().time()
+    for computer in computers_data:
+        if computer['name'] == computer_name:
+            for period in computer['periods']:
+                if is_time_within_period(period['startTime'], period['endTime'], current_time):
+                    return True
+    return False
+
+def main():
+    computers_data = fetch_json_from_github(GITHUB_RAW_URL)
+
+    print(computers_data)
+
+    computer_name = socket.gethostname()
+    print(computer_name)
+
+    if should_shutdown(computers_data, computer_name):
+        print("Shutting down...")
+        os.system('shutdown now -h')
+
+if __name__ == "__main__":
+    main()
